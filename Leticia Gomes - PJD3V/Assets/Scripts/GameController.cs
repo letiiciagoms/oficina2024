c287c2b1dc485babedfa65b34f3decbe078523a3
letiiciagoms <145374951+letiiciagoms@users.noreply.github.com>
2024-08-19 14:23:40 -0300

    v6

--- Leticia Gomes - PJD3V/Assets/Scripts/GameController.cs
+++ Leticia Gomes - PJD3V/Assets/Scripts/GameController.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     public int totalScore;
+    public Text scoreText;
 
     public static GameController instance;
     // Start is called before the first frame update
@@ -13,9 +15,9 @@ public class GameController : MonoBehaviour
         instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateScoreText()
     {
-        
+        scoreText.text = totalScore.ToString();
     }
+
 }
