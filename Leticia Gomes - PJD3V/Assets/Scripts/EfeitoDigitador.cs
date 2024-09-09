38db4a7e077814272d4ba226d92b887d36ff308d
letiiciagoms <145374951+letiiciagoms@users.noreply.github.com>
2024-09-09 13:56:19 -0300

    cutscene2

--- Leticia Gomes - PJD3V/Assets/Scripts/EfeitoDigitador.cs
+++ Leticia Gomes - PJD3V/Assets/Scripts/EfeitoDigitador.cs
@@ -1,37 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
 
 public class EfeitoDigitador : MonoBehaviour
 {
-    private TextMeshProUGUI componentTexto;
-    private AudioSource audioSource;
+    private TextMeshProUGUI componenteTexto;
+    private AudioSource _audioSource;
+    private string mensagemOriginal; 
+    public bool imprimindo;
+    public float tempoEntreLetras = 0.08f;
 
     private void Awake()
     {
-
+        TryGetComponent(out componenteTexto);
+        TryGetComponent(out _audioSource);
+        mensagemOriginal = componenteTexto.text;
+        componenteTexto.text = "";
     }
-
+     
     private void OnEnable()
     {
-
+        ImprimirMensagem(mensagemOriginal);
     }
 
     private void OnDisable()
     {
-
+        componenteTexto.text = mensagemOriginal;
+        StopAllCoroutines();
     }
 
-    public ImprimirMensagem(string msg)
+    public void ImprimirMensagem(string mensagem)
     {
-
+        if (gameObject.activeInHierarchy)
+        {
+            if (imprimindo) return;
+            imprimindo = true;
+            StartCoroutine(LetraPorLetra(mensagem));
+        }
     }
 
-    IEnumerator LetraPorLetra (string msg)
+    IEnumerator LetraPorLetra (string mensagem)
     {
-        yield return null;
+        string msg = "";
+        foreach (var letra in mensagem)
+        {
+            msg += letra;
+            componenteTexto.text = msg;
+            _audioSource.Play();
+            yield return new WaitForSeconds(tempoEntreLetras);
+        }
+
+        imprimindo = false;
+        StopAllCoroutines();
     }
     
 }
